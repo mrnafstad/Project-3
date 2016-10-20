9eb9c51a82c28e2ea18171beb862da53fdddf5a0
mrnafstad <mr.nafstad@gmail.com>
2016-10-20 18:16:32 +0200

    Finished vv, cant test yet...

--- Code/OOB/solver.cpp
+++ Code/OOB/solver.cpp
@@ -34,23 +34,72 @@ void solver::addM(planet newplanet)
     all_planets.push_back(newplanet);
 }
 
-void velVerlet( int dim, int N, double final_time )
+void solver::print_position(std::ofstream &output, int dimension, double time,int number)
+{   // Writes mass, position and velocity to a file "output"
+    if(dimension > 3 || dimension <= 0) dimension = 3;
+    else{
+        for(int i=0;i<number;i++){
+            planet &current = all_planets[i];
+            output << time << "\t" << i+1 << "\t" << current.mass;
+            for(int j=0;j<dimension;j++) output << "\t" << current.position[j];
+            //for(int j=0;j<dimension;j++) output << "\t" << current.velocity[j];
+            output << std::endl;
+        }
+    }
+}
+
+void solver::velVerlet( int dim, int N, double final_time, int print_number )
 {
 	//Will only work for binary system atm
 	double h = final_time/(double)N;
 	double time = 0.0;
+	double ** acceleration = setup_matrix(total_planets, 3);
+	double ** acceleration_new = setup_matrix(total_planets, 3);
+
+	double Fx, Fy, Fz, Fxnew, Fynew, Fznew;
+
+	// Create files for data storage
+    char *filename = new char[1000];
+
+    sprintf( filename, "clusterVV_%d_%.3f.txt", total_planets, h );
+    std::ofstream output_file(filename);
+
+	print_position( output_file, time, print_number )
+
 	while ( time < final_time) {
 		for (int nr1 = 0; nr1 < total_planets; nr1++) {
-			planet &current = all_planets[nr1];
+			planet &current = all_planets[nr2];
 			Fx = Fy = Fz = Fxnew = Fynew = Fznew = 0.0;
 			for ( int nr2 = nr1 + 1; nr2 < total_planets; nr2++) {
 				planet &other = all_planets[nr2];
 				GravitationalForce(other, current, Fx, Fy, Fz);
 			}
 			//next define acceleration, then the real algo
+			acceleration[nr1][0] = Fx/current.mass;
+			acceleration[nr1][1] = Fy/current.mass;
+			acceleration[nr1][2] = Fz/current.mass;
+			for ( int j = 0; j < dim; j++ ) {
+				current.position[j] += h*current.velocity[j] + 0.5*h*h*acceleration[nr1][j];
+			}
 
+			for ( int nr2 = nr1 + 1; nr2 < total_planets; nr2++) {
+				planet &other = all_planets[nr2];
+				GravitationalForce(other, current, Fxnew, Fynew, Fznew);
+			}
+			//next define acceleration, then the real algo
+			acceleration_new[nr1][0] = Fx/current.mass;
+			acceleration_new[nr1][1] = Fy/current.mass;
+			acceleration_new[nr1][2] = Fz/current.mass;
+			for ( int j = 0; j < dim; j++ ) {
+				current.velocity[j] += 0.5*h*h*( acceleration[nr1][j] + acceleration_new[nr1][j]);
+			}
 		}
-	}	
+		time += h;
+		print_position(output_file, dim, time, print_number );
+		
+
+	}
+	output_file.close();	
 }
 
 double ** solver::setup_matrix(int height,int width)
--- Code/OOB/system.cpp
+++ Code/OOB/system.cpp
@@ -1,5 +1,14 @@
-#include "solver.h"
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <time.h>
 #include "planet.h"
+#include "solver.h"
+using namespace std;
 
 int main(){
 
@@ -9,17 +18,24 @@ int main(){
 	double Pos_init[3], Vel_init[3];
 
 	planet Earth(0.000003, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	planet Sun(1, 0, 0, 0, 0, 0, 0);
+	planet Sun(1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 
 	/*for(int i = 0; i < dim; i++){
 		Pos_init[0, i] = Earth.position[i];
 		Vel_init[0, i] = Earth.velocity[i];
 	}*/
 
+	/*
 	solver earthsun_Euler;
 	earthsun_Euler.add(Earth);
-	earthsun_Euler.add(Sun)
+	earthsun_Euler.add(Sun);
+	*/
 
+	solver earthsun_VV();
+	earthsun_VV.add(Earth);
+	earthsun_VV.add(Sun);
 
+	earthsun_VV.velVerlet( 3, 100000, 5.0, 1);
 
+	return 0;
 }

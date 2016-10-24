acb01ea2680105fdf2916b804ffb85f353778325
mrnafstad <mr.nafstad@gmail.com>
2016-10-24 08:39:26 +0200

    check wether you think innersolarwofullrot.png is ait, if not we make a better plot but its better than it was *fistbumps*

--- Code/OOB/solver.cpp
+++ Code/OOB/solver.cpp
@@ -88,7 +88,7 @@ void solver::velVerlet( int dim, int N, double final_time, bool energy, bool sta
 		for ( int i = 1; i < total_planets; i++ ) {
 			for (int k = 0; k < dim; k++ ) {
 				planet &thisother = all_planets[i];
-				sun.velocity[k] += thisother.velocity[k]*thisother.mass/sun.mass;
+				sun.velocity[k] -= thisother.velocity[k]*thisother.mass/sun.mass;
 			}
 		}
 	}
